0d2a9e0121fb145cd63d1b79dcd828d2b7d5164e
Lani0 <paula.chavesrivera@ucr.ac.cr>
2025-05-17 18:06:10 -0600

    Diseño inicial FE

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -4,12 +4,15 @@ import VerificationPage from './pages/verificationPyme';
 import { PasswordResetPage } from './pages/PasswordResetPage';
 import ProductPublishForm from './pages/publishProduct';
 import ProductPublishPanel from './pages/manageProducts';
+import Home from './pages/home';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
 	return (
 		<Router>
 			<div className='app'>
 				<Routes>
+					<Route path='/' element={<Home />} />
 					<Route path='/registro' element={<RegisterPymePage />} />
 					<Route path='/verificacion' element={<VerificationPage />} />
 					<Route path='/reset-password/:token' element={<PasswordResetPage />} />

8da645e1e5840ed6fef67dfc2d223aa52d4cfa2f
Marianoda20027 <163364385+Marianoda20027@users.noreply.github.com>
2025-06-26 20:15:13 -0600

    Se implemento el servicio de recomendacion

--- front-end/src/services/clients.service.ts
+++ front-end/src/services/clients.service.ts
@@ -1,6 +1,8 @@
-// Crea instancias de API configuradas para los servicios de ventas y autenticación
-import { createApiInstance } from './http.service';
-import { createApiMethods } from './http.service';
+// src/services/clients.service.ts
 
+import { createApiInstance, createApiMethods } from './http.service';
+
+// Clientes de API para cada microservicio
 export const ventasApi = createApiMethods(createApiInstance('http://localhost:8082'));
 export const authApi = createApiMethods(createApiInstance('http://localhost:8083'));
+export const recommendationApi = createApiMethods(createApiInstance('http://localhost:8081'));
